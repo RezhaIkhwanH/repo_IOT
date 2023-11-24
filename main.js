5b41aea781a4a0a45bc1e681220e3b1b7065d982
rezhaA22 <rezhaikwanh@students.amikom.ac.id>
2023-11-24 11:57:14 +0700

    awal

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cros = require("cors");
+const app = express();
+const db = require('@cyclic.sh/dynamodb');
+
+
+app.use(cros());
+app.use(express.json(true));
+const data = db.collection("data");
+const seting = db.collection("seting");
+
+// (async function () {
+//     let setingAwal = await seting.set("setingAwal", {
+//         suhu: "30",
+//         kelembapan: "50",
+//         cahaya: "500"
+//     })
+// })();
+(function () {
+    console.log('IIFE');
+})();
+
+app.get('/', async function (req, res) {
+    let item = await seting.get("setingAwal")
+    res.send(item)
+})
+
+app.listen(8080, () => {
+    console.log(`Example app listening at http://localhost:${8080}`);
+});
