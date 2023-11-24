a663304c0a1d3bbe7709e3b4eb3b3ee5d27501f4
rezhaA22 <rezhaikwanh@students.amikom.ac.id>
2023-11-24 12:52:17 +0700

    5

--- main.js
+++ main.js
@@ -9,25 +9,68 @@ app.use(express.json(true));
 const data = db.collection("data");
 const seting = db.collection("seting");
 
+
 // (async function () {
-//     let setingAwal = await seting.set("setingAwal", {
+//     await seting.set("setingAwal", {
 //         suhu: "30",
 //         kelembapan: "50",
 //         cahaya: "500"
 //     })
 // })();
-(async function () {
-    await seting.set("setingAwal", {
-        suhu: "30",
-        kelembapan: "50",
-        cahaya: "500"
-    })
-})();
 
 app.get('/', async function (req, res) {
     let item = await seting.get("setingAwal")
     console.log(item);
-    res.send({ pesan: "api ok", data: item })
+    res.send({ pesan: "api ok", data: item.props })
+})
+
+app.get("/getData", async function (req, res) {
+    res.send(db.collection("data"));
+})
+app.post("/createdData", async function (req, res) {
+
+    const suhu = req.body.suhu;
+    const cahaya = req.body.cahaya;
+    const kelembapan = req.body.kelembapan;
+    const kelembapanUdara = req.body.kelembapanUdara;
+    const key = new Date().getTime().toString();
+    try {
+        await data.set(key, {
+            suhu,
+            kelembapan,
+            kelembapanUdara,
+            cahaya
+        })
+        res.send({ pesan: "berhasil", data: (await data.get(key)).props })
+
+    } catch (error) {
+        res.status(500).send({ pesan: "gagal" })
+
+    }
+
+})
+
+app.post('/createdSeting', async function (req, res) {
+    // 
+    const suhu = req.body.suhu;
+    const cahaya = req.body.cahaya;
+    const kelembapan = req.body.kelembapan;
+    await seting.set("seting", {
+        suhu,
+        kelembapan,
+        cahaya
+    })
+    res.send({ pesan: "seting save", data: req.body })
+})
+
+app.get('/getSeting', async function (req, res) {
+    let item = await seting.get("seting")
+    res.send({ pesan: "api ok", data: item.props })
+})
+
+app.get('/getSetingAwal', async function (req, res) {
+    let item = await seting.get("setingAwal")
+    res.send({ pesan: "api ok", data: item.props })
 })
 
 app.listen(8080, () => {
